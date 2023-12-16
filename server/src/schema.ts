f6b00a7e081810604dee70c2ec2b9dfc9935c324
emilie <loveskyship@gmail.com>
2023-12-16 10:09:46 -0800

    type definition of Mutation - addBook

--- server/src/schema.ts
+++ server/src/schema.ts
@@ -1,33 +1,41 @@
 const typeDefs = `
-type Book {
-  title: String!
-  published: Int!
-  author: Author!
-  genres: [String!]!
-  id: ID!
-}
+  type Book {
+    title: String!
+    published: Int!
+    author: Author!
+    genres: [String!]!
+    id: ID!
+  }
 
-type Author {
-  name: String!
-  born: Int
-  bookCount: Int!
-  books: [Book!]!
-  id: ID!
-}
+  type Author {
+    name: String!
+    born: Int
+    bookCount: Int!
+    books: [Book!]!
+    id: ID!
+  }
 
-type User {
-  username: String!
-  favoriteGenre: String!
-  id: ID!
-}
+  type User {
+    username: String!
+    favoriteGenre: String!
+    id: ID!
+  }
 
-type Query {
-  bookCount: Int!
-  authorCount: Int!
-  allBooks(author: String, genre: String): [Book!]!
-  allAuthors: [Author!]!
-}
+  type Query {
+    bookCount: Int!
+    authorCount: Int!
+    allBooks(author: String, genre: String): [Book!]!
+    allAuthors: [Author!]!
+  }
 
+  type Mutation {
+    addBook (
+      title: String!
+      published: Int!
+      author: String!
+      genres: [String!]!
+    ) : Book
+  }
 `;
 
 export default typeDefs;
