9660a5ce62bf7b4ee9082854d57eacce2e0db66f
emilie <loveskyship@gmail.com>
2023-12-16 09:32:09 -0800

    type definition of Author

--- server/src/schema.ts
+++ server/src/schema.ts
@@ -2,10 +2,19 @@ const typeDefs = `
 type Book {
   title: String!
   published: Int!
-  author: String
+  author: Author!
   genres: [String!]!
   id: ID!
 }
+
+type Author {
+  name: String!
+  born: Int
+  bookCount: Int!
+  books: [Book!]!
+  id: ID!
+}
+
 `;
 
 export default typeDefs;
