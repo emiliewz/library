f22c2dfa22c76ae7537dec0c142412df272e1850
emilie <loveskyship@gmail.com>
2023-12-16 12:06:02 -0800

    implement author model

---
+++ server/src/models/author.ts
@@ -0,0 +1,38 @@
+import { Schema, model } from 'mongoose';
+import uniqueValidator from 'mongoose-unique-validator';
+import { IAuthor } from '../types';
+
+const authorSchema = new Schema<IAuthor>({
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+    minLength: 4
+  },
+  born: {
+    type: Number,
+  },
+  books: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'Book'
+    }
+  ],
+  bookCount: {
+    type: Number,
+    default: 0
+  }
+});
+
+authorSchema.plugin(uniqueValidator);
+
+authorSchema.set('toJSON', {
+  transform: (_doc, ret: Record<string, string>) => {
+    ret.id = ret._id;
+    delete ret._id;
+    delete ret.__v;
+  },
+});
+const Author = model<IAuthor>('Author', authorSchema);
+
+export default Author;
