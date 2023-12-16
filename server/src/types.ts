4d104caa26e9d94a50b41ffb4bb87361db097c28
emilie <loveskyship@gmail.com>
2023-12-16 12:04:22 -0800

    define IAuthor interface

--- server/src/types.ts
+++ server/src/types.ts
@@ -1,5 +1,12 @@
 import { Types } from 'mongoose';
 
+export interface IAuthor {
+  name: string
+  born: number
+  bookCount: number
+  books: Types.ObjectId[]
+}
+
 export interface IBook {
   title: string
   published: number
