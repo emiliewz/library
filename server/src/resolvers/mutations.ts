76b0903ddbeb98d096cea8880041283cadd67100
emilie <loveskyship@gmail.com>
2023-12-18 19:29:56 -0800

    defint mutations - create a user

--- server/src/resolvers/mutations.ts
+++ server/src/resolvers/mutations.ts
@@ -3,7 +3,9 @@ import { MutationResolvers } from '../__generated__/resolvers-types';
 import Author from '../models/author';
 import Book from '../models/book';
 import { HydratedDocument } from 'mongoose';
-import { IBook } from '../types';
+import { IBook, IUser } from '../types';
+import User from '../models/user';
+import bcrypt from 'bcrypt';
 
 const mutations: MutationResolvers = {
   addBook: async (_root, { title, published, author, genres }) => {
@@ -63,6 +65,34 @@ const mutations: MutationResolvers = {
         invalidArgs: name
       }
     });
+  },
+
+  createUser: async (_root, { username, favoriteGenre, name, password }) => {
+    if (!password || password.length < 6) {
+      throw new GraphQLError('`password` is shorter than the minimum allowed length (6)', {
+        extensions: {
+          code: 'BAD_USER_INPUT',
+          invalidArgs: username && favoriteGenre,
+        }
+      });
+    }
+
+    const saltRounds: number = 10;
+    const passwordHash: string = await bcrypt.hash(password, saltRounds);
+
+    const user: HydratedDocument<IUser> = new User({ username, favoriteGenre, name, passwordHash });
+    try {
+      await user.save();
+      return User.findById(user.id);
+    } catch (error) {
+      throw new GraphQLError('Creating the user failed', {
+        extensions: {
+          code: 'BAD_USER_INPUT',
+          invalidArgs: error,
+          error
+        }
+      });
+    }
   }
 };
 
