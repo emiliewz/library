7a54d4ef04f1f869d14f9140193272c2e3066201
emilie <loveskyship@gmail.com>
2023-12-18 19:42:50 -0800

    define resolvers map

---
+++ server/src/resolvers/index.ts
@@ -0,0 +1,9 @@
+import { Resolvers } from '../__generated__/resolvers-types';
+import Query from './queries';
+import Mutation from './mutations';
+
+const resolvers: Resolvers = {
+  Query, Mutation
+};
+
+export default resolvers;
