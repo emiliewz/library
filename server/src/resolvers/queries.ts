6c44cb98d0d38f1cfa44b299059c90899ca24b96
emilie <loveskyship@gmail.com>
2023-12-18 19:24:39 -0800

    query - get all books

--- server/src/resolvers/queries.ts
+++ server/src/resolvers/queries.ts
@@ -2,9 +2,25 @@ import { QueryResolvers } from '../__generated__/resolvers-types';
 import Author from '../models/author';
 import Book from '../models/book';
 
+type AllBooksQuery = {
+  author: string | undefined
+  genres: string | undefined
+};
+
 const queries: QueryResolvers = {
   bookCount: async () => Book.collection.countDocuments(),
-  authorCount: async () => Author.collection.countDocuments()
+  authorCount: async () => Author.collection.countDocuments(),
+  allBooks: async (_root, { author, genre }) => {
+    let query = {} as AllBooksQuery;
+    if (author) {
+      const authorOfBook = await Author.findOne({ name: author });
+      query = { ...query, author: authorOfBook?._id.toString() };
+    }
+    if (genre) {
+      query = { ...query, genres: genre };
+    }
+    return Book.find(query).populate('author');
+  }
 };
 
 export default queries;
