d18a5891d9f2471d9b12ebf35224020bcbb87156
emilie <loveskyship@gmail.com>
2023-12-19 13:14:58 -0800

    pass context function to integration function

--- server/src/index.ts
+++ server/src/index.ts
@@ -5,6 +5,7 @@ import { startStandaloneServer } from '@apollo/server/standalone';
 import mongoose from 'mongoose';
 import config from './utils/config';
 import { User } from './__generated__/resolvers-types';
+import context from './context';
 
 const typeDefs = readFileSync('./src/schema.graphql', { encoding: 'utf-8' });
 
@@ -12,7 +13,7 @@ export interface MyContext {
   user?: User
 }
 
-const server = new ApolloServer({
+const server = new ApolloServer<MyContext>({
   typeDefs,
   resolvers,
 });
@@ -22,7 +23,10 @@ mongoose.connect(config.MONGODB_URI)
   .then(() => console.log('connected to MongoDB'))
   .catch(error => console.log('error connecting to MongoDB:', error.message));
 
-startStandaloneServer(server, { listen: { port: config.PORT } })
+startStandaloneServer(server, {
+  listen: { port: config.PORT },
+  context
+})
   .then(({ url }) => console.log(`Server ready at ${url}`))
   .catch(error => console.log(error.message));
 
