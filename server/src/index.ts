1ab4b266f76633b6ebb775ae80e3534b5da87f7a
emilie <loveskyship@gmail.com>
2023-12-18 19:45:31 -0800

    create an instance of apolloServer

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,23 @@
+import resolvers from './resolvers';
+import { ApolloServer, BaseContext } from '@apollo/server';
+import { readFileSync } from 'fs';
+import { startStandaloneServer } from '@apollo/server/standalone';
 import mongoose from 'mongoose';
 import config from './utils/config';
+
+const typeDefs = readFileSync('./src/schema.graphql', { encoding: 'utf-8' });
+
+const server: ApolloServer<BaseContext> = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
 
 mongoose.set('strictQuery', false);
 mongoose.connect(config.MONGODB_URI)
   .then(() => console.log('connected to MongoDB'))
   .catch(error => console.log('error connecting to MongoDB:', error.message));
+
+startStandaloneServer(server, { listen: { port: config.PORT } })
+  .then(({ url }) => console.log(`Server ready at ${url}`))
+  .catch(error => console.log(error.message));
+
