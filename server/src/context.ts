dc05d7e75466e7d0d96ea4cabbe533b610baa829
emilie <loveskyship@gmail.com>
2023-12-19 13:11:53 -0800

    extract user from token

--- server/src/context.ts
+++ server/src/context.ts
@@ -1,10 +1,36 @@
+import User from './models/user';
+import jwt from 'jsonwebtoken';
+import config from './utils/config';
+import { GraphQLError } from 'graphql';
 import { IncomingMessage } from 'http';
 
-export const getTokenFromRequest = (req: IncomingMessage): string | null => {
+const getTokenFromRequest = (req: IncomingMessage): string | null => {
   const authorization = req.headers.authorization;
 
   const token = authorization && authorization.startsWith('Bearer ')
     ? authorization.replace('Bearer ', '')
     : null;
   return token;
+};
+
+export const getUserFromRequest = async (req: IncomingMessage) => {
+  const token = getTokenFromRequest(req);
+
+  try {
+    if (token) {
+      const decodedToken = <jwt.UserForTokenPayload>jwt.verify(token, config.SECRET);
+      if (decodedToken.id) {
+        const user = await User.findById(decodedToken.id);
+        return user;
+      }
+    }
+    return null;
+  } catch (error) {
+    throw new GraphQLError('invalid token', {
+      extensions: {
+        code: 'BAD_USER_INPUT',
+        error
+      }
+    });
+  }
 };
