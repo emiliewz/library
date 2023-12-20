97b4a5ad10a2595f404edda0b9926a94361b55ea
emilie <loveskyship@gmail.com>
2023-12-19 23:40:11 -0800

    implement Authors component

---
+++ client/src/app/App.tsx
@@ -0,0 +1,12 @@
+import Authors from '../components/Authors';
+
+const App = () => {
+  return (
+    <div>
+      <h2>Library App</h2>
+      <Authors />
+    </div>
+  );
+};
+
+export default App;
