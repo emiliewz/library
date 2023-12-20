e2b5c4047ba9337157908e26330463689be15890
emilie <loveskyship@gmail.com>
2023-12-19 21:34:19 -0800

    run both an Express app and a separate WebSocket server for subscriptions

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,32 +1,73 @@
 import resolvers from './resolvers';
 import { ApolloServer } from '@apollo/server';
 import { readFileSync } from 'fs';
-import { startStandaloneServer } from '@apollo/server/standalone';
 import mongoose from 'mongoose';
 import config from './utils/config';
 import { User } from './__generated__/resolvers-types';
 import context from './context';
 
+import { WebSocketServer } from 'ws';
+import { createServer } from 'http';
+import { makeExecutableSchema } from '@graphql-tools/schema';
+import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer';
+import { useServer } from 'graphql-ws/lib/use/ws';
+import { expressMiddleware } from '@apollo/server/express4';
+import cors from 'cors';
+import express from 'express';
+
 const typeDefs = readFileSync('./src/schema.graphql', { encoding: 'utf-8' });
 
 export interface MyContext {
-  user?: User
+  user: User
 }
-
-const server = new ApolloServer<MyContext>({
-  typeDefs,
-  resolvers,
-});
 
 mongoose.set('strictQuery', false);
 mongoose.connect(config.MONGODB_URI)
   .then(() => console.log('connected to MongoDB'))
   .catch(error => console.log('error connecting to MongoDB:', error.message));
 
-startStandaloneServer(server, {
-  listen: { port: config.PORT },
-  context
-})
-  .then(({ url }) => console.log(`Server ready at ${url}`))
-  .catch(error => console.log(error.message));
+const main = async () => {
+  const app = express();
+  const httpServer = createServer(app);
 
+  const wsServer = new WebSocketServer({
+    server: httpServer,
+    path: '/',
+  });
+
+  const schema = makeExecutableSchema({ typeDefs, resolvers });
+  const serverCleanup = useServer({ schema }, wsServer);
+
+  const server = new ApolloServer({
+    schema,
+    plugins: [
+      ApolloServerPluginDrainHttpServer({ httpServer }),
+      {
+        // eslint-disable-next-line @typescript-eslint/require-await
+        async serverWillStart() {
+          return {
+            async drainServer() {
+              await serverCleanup.dispose();
+            }
+          };
+        }
+      }
+    ]
+  });
+
+  await server.start();
+
+  app.use(
+    '/',
+    cors(),
+    express.static(__dirname + '/dist'),
+    express.json(),
+    expressMiddleware(server, { context })
+  );
+
+  httpServer.listen(config.PORT, () =>
+    console.log(`Server is now running on http://localhost:${config.PORT}`)
+  );
+};
+
+void main();
