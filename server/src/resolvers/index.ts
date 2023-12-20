61706dd6296ebd45b4252c4bba3682e84aee73f0
emilie <loveskyship@gmail.com>
2023-12-19 21:25:37 -0800

    add subscription to the resolvers map

--- server/src/resolvers/index.ts
+++ server/src/resolvers/index.ts
@@ -1,9 +1,10 @@
 import { Resolvers } from '../__generated__/resolvers-types';
 import Query from './queries';
 import Mutation from './mutations';
+import Subscription from './subscriptions';
 
 const resolvers: Resolvers = {
-  Query, Mutation
+  Query, Mutation, Subscription
 };
 
 export default resolvers;
