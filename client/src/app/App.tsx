841b4420bb153fae5296791a67c27030d1909849
emilie <loveskyship@gmail.com>
2023-12-20 22:49:28 -0800

    define add new book route and navigation link

--- client/src/app/App.tsx
+++ client/src/app/App.tsx
@@ -2,6 +2,7 @@ import Authors from '../components/Authors';
 import { Link, Route, Routes } from 'react-router-dom';
 import Login from '../components/Login';
 import Books from '../components/Books';
+import NewBook from '../components/NewBook';
 
 const App = () => {
 
@@ -10,11 +11,13 @@ const App = () => {
       <h2>Library App</h2>
       <Link className='p-2 ps-0 text-decoration-none' to='/authors'>Authors</Link>
       <Link className='p-2 ps-0 text-decoration-none' to='/books'>Books</Link>
+      <Link className='p-2 ps-0 text-decoration-none' to='/create'>Add</Link>
       <Link className='p-2 ps-0 text-decoration-none' to='/login'>Login</Link>
 
       <Routes>
         <Route path='/authors' element={<Authors />} />
         <Route path='/books' element={<Books />} />
+        <Route path='/create' element={<NewBook />} />
         <Route path='/login' element={<Login />} />
       </Routes>
     </div>
