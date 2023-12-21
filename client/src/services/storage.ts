41b1012b7e981e327de7dba9ced77e09e939117d
emilie <loveskyship@gmail.com>
2023-12-20 17:20:39 -0800

    define get user, and get token function

--- client/src/services/storage.ts
+++ client/src/services/storage.ts
@@ -6,8 +6,21 @@ const saveUser = (u: User): void => {
   localStorage.setItem(KEY, JSON.stringify(u));
 };
 
+const getUser = (): User | null => {
+  const user = localStorage.getItem(KEY);
+  if (user) {
+    return JSON.parse(user);
+  }
+  return null;
+};
+
+const getToken = (): string | undefined => {
+  const user = getUser();
+  return user?.token;
+};
+
 const clearUser = (): void => localStorage.removeItem(KEY);
 
 export default {
-  saveUser, clearUser
+  saveUser, getUser, clearUser, getToken
 };
