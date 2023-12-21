2b0fa2e9c5d23f0b4fdb3f08d25d630168b388f9
emilie <loveskyship@gmail.com>
2023-12-20 21:03:01 -0800

    define books route and navigation link

--- client/src/app/App.tsx
+++ client/src/app/App.tsx
@@ -1,6 +1,7 @@
 import Authors from '../components/Authors';
 import { Link, Route, Routes } from 'react-router-dom';
 import Login from '../components/Login';
+import Books from '../components/Books';
 
 const App = () => {
 
@@ -8,10 +9,12 @@ const App = () => {
     <div className='container'>
       <h2>Library App</h2>
       <Link className='p-2 ps-0 text-decoration-none' to='/authors'>Authors</Link>
+      <Link className='p-2 ps-0 text-decoration-none' to='/books'>Books</Link>
       <Link className='p-2 ps-0 text-decoration-none' to='/login'>Login</Link>
 
       <Routes>
         <Route path='/authors' element={<Authors />} />
+        <Route path='/books' element={<Books />} />
         <Route path='/login' element={<Login />} />
       </Routes>
     </div>
