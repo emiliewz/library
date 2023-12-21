674f284631e9cc445c8411c727dc01a45982ea6f
emilie <loveskyship@gmail.com>
2023-12-20 18:14:21 -0800

    define login route and navigation link

--- client/src/app/App.tsx
+++ client/src/app/App.tsx
@@ -1,5 +1,6 @@
 import Authors from '../components/Authors';
 import { Link, Route, Routes } from 'react-router-dom';
+import Login from '../components/Login';
 
 const App = () => {
 
@@ -7,9 +8,11 @@ const App = () => {
     <div className='container'>
       <h2>Library App</h2>
       <Link className='p-2 ps-0 text-decoration-none' to='/authors'>Authors</Link>
+      <Link className='p-2 ps-0 text-decoration-none' to='/login'>Login</Link>
 
       <Routes>
         <Route path='/authors' element={<Authors />} />
+        <Route path='/login' element={<Login />} />
       </Routes>
     </div>
   );
