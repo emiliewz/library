b1278303f83a324fbb8504465435aba423fab9cb
emilie <loveskyship@gmail.com>
2023-12-20 17:36:28 -0800

    define authors route and navigation link

--- client/src/app/App.tsx
+++ client/src/app/App.tsx
@@ -1,10 +1,16 @@
 import Authors from '../components/Authors';
+import { Link, Route, Routes } from 'react-router-dom';
 
 const App = () => {
+
   return (
-    <div>
+    <div className='container'>
       <h2>Library App</h2>
-      <Authors />
+      <Link className='p-2 ps-0 text-decoration-none' to='/authors'>Authors</Link>
+
+      <Routes>
+        <Route path='/authors' element={<Authors />} />
+      </Routes>
     </div>
   );
 };
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,6 +2,7 @@ import ReactDOM from 'react-dom/client';
 import App from './app/App';
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 import { getSplitLink } from './utils/index';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 const client = new ApolloClient({
   link: getSplitLink(),
@@ -10,6 +11,8 @@ const client = new ApolloClient({
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <ApolloProvider client={client}>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </ApolloProvider>
 );
