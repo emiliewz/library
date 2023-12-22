9f2bdfa3b4eeaec203628a2423e28fea118cc6b5
emilie <loveskyship@gmail.com>
2023-12-21 19:22:45 -0800

    define Notification component

---
+++ client/src/components/Notification.tsx
@@ -0,0 +1,17 @@
+import { Alert } from 'react-bootstrap';
+import { Info } from '../app/type';
+
+const Notification = ({ info }: { info: Info }) => {
+
+  if (!info.message) {
+    return null;
+  }
+
+  return (
+    <Alert variant={info.type === 'error' ? 'warning' : 'info'}>
+      {info.message}
+    </Alert >
+  );
+};
+
+export default Notification;
